2814e2a03b0e8950429f66a3a6b26fb48f450c17
ZyCromerZ <neetroid97@gmail.com>
2020-09-14 11:16:30 +0700

    dump: miui_BEGONIAGlobal_V12.0.3.0.QGGMIXM_4bd96ae3c3_10.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+Link="https://shared.zyc-files.workers.dev/0:/miui_BEGONIAGlobal_V12.0.3.0.QGGMIXM_4bd96ae3c3_10.0.zip"
+FileName="miui_BEGONIAGlobal_V12.0.3.0.QGGMIXM_4bd96ae3c3_10.0.zip"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
-wget "https://shared.zyc-files.workers.dev/0:/miui_BEGONIA_20.9.10_c84222bc2b_10.0.zip" -O miui_BEGONIA_20.9.10_c84222bc2b_10.0.zip
-bash dumpyara.sh miui_BEGONIA_20.9.10_c84222bc2b_10.0.zip "$GIT_SECRET" "$BOT_TOKEN"
+wget "$Link" -O "$FileName"
+bash dumpyara.sh "$FileName" "$GIT_SECRET" "$BOT_TOKEN"

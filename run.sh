6c5e447ee30d4bbb17315552c68f62d96d2ea278
ZyCromerZ <neetroid97@gmail.com>
2020-09-15 08:15:42 +0700

    dump: begonia_global_images_V12.0.3.0.QGGMIXM_20200902.0000.00_10.0_global_abde420a62.tgz
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-Link="https://shared.zyc-files.workers.dev/0:/miui_BEGONIAIDGlobal_V12.0.1.0.QGGIDXM_d0e8262216_10.0.zip"
-FileName="miui_BEGONIAIDGlobal_V12.0.1.0.QGGIDXM_d0e8262216_10.0.zip"
+Link="https://shared.zyc-files.workers.dev/0:/begonia_global_images_V12.0.3.0.QGGMIXM_20200902.0000.00_10.0_global_abde420a62.tgz"
+FileName="begonia_global_images_V12.0.3.0.QGGMIXM_20200902.0000.00_10.0_global_abde420a62.tgz"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
-wget "$Link" -O "$FileName"
+echo "downloading files"
+wget "$Link" -O "$FileName" >/dev/null
+echo "downloading files done . . ."
 bash dumpyara.sh "$FileName" "$GIT_SECRET" "$BOT_TOKEN"

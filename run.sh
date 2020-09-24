273236f4617c6f49184f0e56e194379c6170e0fc
ZyCromerZ <neetroid97@gmail.com>
2020-09-25 05:14:55 +0700

    dump: miui_BEGONIA_20.9.24_939a40bb01_10.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Link="https://shared.zyc-files.workers.dev/0:/miui_BEGONIAININGlobal_V12.0.2.0.QGGINXM_6d7b066360_10.0.zip"
-FileName="miui_BEGONIAININGlobal_V12.0.2.0.QGGINXM_6d7b066360_10.0.zip"
+Link="http://bigota.d.miui.com/20.9.24/miui_BEGONIA_20.9.24_939a40bb01_10.0.zip"
+FileName="miui_BEGONIA_20.9.24_939a40bb01_10.0.zip"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
 wget "$Link" -O "$FileName"

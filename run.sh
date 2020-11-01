5d0075a116c1c26616dff4750ca2db8bc859a71d
ZyCromerZ <neetroid97@gmail.com>
2020-11-01 21:42:14 +0700

    dump: miui_BEGONIA_20.10.30_c34625a5ce_10.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Link="https://hugeota.d.miui.com/20.10.29/miui_BEGONIA_20.10.29_04b140f215_10.0.zip"
-FileName="$(pwd)/miui_BEGONIA_20.10.29_04b140f215_10.0.zip"
+Link="https://hugeota.d.miui.com/20.10.30/miui_BEGONIA_20.10.30_c34625a5ce_10.0.zip"
+FileName="$(pwd)/miui_BEGONIA_20.10.30_c34625a5ce_10.0.zip"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
 

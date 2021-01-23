5ab49a446dd130154bfd652820728f322660ca6e
ZyCromerZ <neetroid97@gmail.com>
2021-01-23 15:13:45 +0700

    dump: miui_BEGONIA_21.1.20_db50c76261_11.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-Link="https://shared.zyc-files.workers.dev/0:/miui_BEGONIA_20.11.18_87476c1dbd_10.0.zip"
-FileName="$(pwd)/miui_BEGONIA_20.11.18_87476c1dbd_10.0.zip"
+Link="https://shared.zyc-files.workers.dev/0:/miui_BEGONIA_21.1.20_db50c76261_11.0.zip"
+FileName="$(pwd)/memeui.zip"
+export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
 

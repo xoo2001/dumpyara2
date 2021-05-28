feebebf4db266b17c338d1087965b0a2418f7370
ZyCromerZ <neetroid97@gmail.com>
2021-05-28 12:53:18 +0700

    dump: miui_VAYUEEAGlobal_V12.5.1.0.RJUEUXM_8f1ef5258e_11.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Link="https://gcc-drive.zyc-files.workers.dev/1:/miui_VAYUIDGlobal_V12.0.3.0.RJUIDXM_1511576317_11.0.zip"
+Link="https://gcc-drive.zyc-files.workers.dev/1:/miui_VAYUEEAGlobal_V12.5.1.0.RJUEUXM_8f1ef5258e_11.0.zip"
 FileName="$(pwd)/memeui.zip"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget

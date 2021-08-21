4f7788dbe72e1859172b4115ac254ad3c1052cae
ZyCromerZ <neetroid97@gmail.com>
2021-08-21 11:44:58 +0700

    dump: miui_BEGONIAIDGlobal_V12.5.2.0.RGGIDXM_c690ecd789_11.0.zip
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-Link="https://gcc-drive.zyc-files.workers.dev/1:/miui_VAYUGlobal_V12.5.3.0.RJUMIXM_8c9f40a764_11.0.zip"
+Link="https://gcc-drive.zyc-files.workers.dev/1:/miui_BEGONIAIDGlobal_V12.5.2.0.RGGIDXM_c690ecd789_11.0.zip"
+GDid="10pNyWcDJ2pQSwKFB_Rm5VF8P__bSlzpm"
 FileName="$(pwd)/memeui.zip"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
@@ -9,7 +10,8 @@ pip3 install backports.lzma protobuf pycrypto docopt
 CurrentFolder=$(pwd)
 
 if [ ! -z "$Link" ];then
-    wget "$Link" -O "$FileName"
+    # wget "$Link" -O "$FileName"
+    wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${GDid} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${GDid}" -O "$FileName" && rm -rf cookies.txt
     bash dumpyara.sh "$FileName" "$GIT_SECRET" "$BOT_TOKEN"
 fi
 
